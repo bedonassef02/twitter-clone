d6d23078d8fe5d2720b3d8958bc88e5844358d2c
Bedo <bedonassef71@gmail.com>
2024-06-13 11:03:22 +0300

    enhance

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     .setDescription('The Twitter API description')
     .setVersion('1.0')
     .addServer('http://localhost:3000/', 'Local environment')
-    .addServer('https://twitter-clone-c0g3.onrender.com/', 'Production')
+    .addServer('https://twitter-api-ld6h.onrender.com/', 'Production')
     .addBearerAuth()
     .build();
 
