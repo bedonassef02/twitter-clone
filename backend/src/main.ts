694c51b9cf0b718f9c11ba0f063b508f4efbad41
Bedo <bedonassef71@gmail.com>
2024-06-13 11:13:21 +0300

    enhance

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,29 +5,33 @@ import { useContainer } from 'class-validator';
 import * as cookieParser from 'cookie-parser';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+      .setTitle('Twitter APP API')
+      .setDescription('The Twitter API description')
+      .setVersion('1.0')
+      .addServer('http://localhost:3000/', 'Local environment')
+      .addServer('https://twitter-api-ld6h.onrender.com/', 'Production')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+      new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
+  app.enableVersioning();
+  app.enableCors();
+  app.use(cookieParser());
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
-  app.use(cookieParser());
+  const port: number = parseInt(process.env.APP_PORT, 10) || 3000;
 
-  const config = new DocumentBuilder()
-    .setTitle('Twitter-API')
-    .setDescription('The Twitter API description')
-    .setVersion('1.0')
-    .addServer('http://localhost:3000/', 'Local environment')
-    .addServer('https://twitter-api-ld6h.onrender.com/', 'Production')
-    .addBearerAuth()
-    .build();
-
-  app.enableCors();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+  await app.listen(port);
 }
+
 bootstrap();
